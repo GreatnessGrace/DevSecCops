759f9806feb17a57c47e2aba89bba5b6af339a4b
GreatnessGrace <graceunstoppable7@gmail.com>
2025-04-11 15:21:57 +0530

    redundency in layout removed

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-      <div className="text-red-800 p-4 bg-red-900 border-l cursor-ew-resize">Test Tailwind</div>
         <Providers>{children}</Providers>
       </body>
     </html>
